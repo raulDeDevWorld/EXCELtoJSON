749b25728722da86e0b3e73645d8708974273bd5
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2024-06-08 16:06:00 -0400

    Update index.js
    DC

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 var XLSX = require("xlsx");
+const { writeFile } = require('fs/promises');
 
 
-// import * as fs from 'fs';
-// XLSX.set_fs(fs);
-
-// /* load 'stream' for stream support */
-// import { Readable } from 'stream';
-// XLSX.stream.set_readable(Readable);
-
-// /* load the codepage support library for extended support with older formats  */
-// import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
-// XLSX.set_cptable(cpexcel);
-
+async function writeToFile(fileName, data) {
+  try {
+    await writeFile(fileName, data);
+    console.log(`Wrote data to ${fileName}`);
+  } catch (error) {
+    console.error(`Got an error trying to write the file: ${error.message}`);
+  }
+}
 
 
 
 
 const toJSON = () => {
     const excel = XLSX.readFile(
-        "C:\\Users\\equipo\\Desktop\\new\\TARIFARIOFTL.xlsx"
+        "C:\\Users\\HOY\\Desktop\\Swoou\\Excel.xlsx"
     )
     let nameHoja= excel.SheetNames
     let datos = XLSX.utils.sheet_to_json(excel.Sheets[nameHoja[0]])
-
-    // for (let i = 0; i < datos.length ; i++) {
-    //     console.log(datos[i])
-        
-    // }
-
-
-    console.log(datos)
+    let db = JSON.stringify(datos)
+    console.log(db)
+ 
+writeToFile('db.json', db);
+    console.log(db)
 }
 toJSON()
